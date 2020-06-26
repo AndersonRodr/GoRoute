ff5e26f296deab7bdea37f9cd8d84aefca627478
Eliana Maria <elianamsfcontatos@gmail.com>
2020-06-26 18:43:11 -0300

    Update __init__.py

--- RotasFrequentes/RotaFrequentesModel/__init__.py
+++ RotasFrequentes/RotaFrequentesModel/__init__.py
@@ -5,8 +5,8 @@ import pandas as pd
 class RotasFrequentes():
     def __init__(self):
         self.rotasFrequentesFrame = pd.DataFrame
-        self.horaSaida = datetime
-        self.horaChegada = datetime
+        self.horaSaida = datetime.time
+        self.horaChegada = datetime.time
         self.delay = 0
 
 
